47da2a279fa45b71f095c19025ebc090bcaef277
Staro <staro.fy.zhao@mail.foxconn.com>
2020-05-08 18:37:55 +0800

    新增了sring cloud config

---
+++ config-center/spring-config-client/src/main/java/spring/cloud/config/springconfigclient/SpringConfigClientApplication.java
@@ -0,0 +1,13 @@
+package spring.cloud.config.springconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConfigClientApplication.class, args);
+    }
+
+}
---
+++ config-center/spring-config-client/src/main/java/spring/cloud/config/springconfigclient/controller/HelloController.java
@@ -0,0 +1,21 @@
+package spring.cloud.config.springconfigclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: Staro
+ * @date: 2020/5/8 18:16
+ * @Description:
+ */
+@RestController
+public class HelloController {
+    @Value("${neo.hello}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String from() {
+        return this.hello;
+    }
+}
