39773f65c26c3ba69acd190cf0105d7bb0780fe3
Staro <staro.fy.zhao@mail.foxconn.com>
2020-05-11 17:45:16 +0800

    添加了高可用的spring-cloud-config 消息总线模块

---
+++ config-center/spring-config-client-mq/src/main/java/spring/cloud/config/springconfigclientmq/controller/HelloController.java
@@ -0,0 +1,23 @@
+package spring.cloud.config.springconfigclientmq.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: Staro
+ * @date: 2020/5/8 18:16
+ * @Description:
+ */
+@RestController
+@RefreshScope
+public class HelloController {
+    @Value("${neo.hello}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String from() {
+        return this.hello;
+    }
+}
---
+++ config-center/spring-config-client-mq/src/test/java/spring/cloud/config/springconfigclientmq/SpringConfigClientMqApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.cloud.config.springconfigclientmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigClientMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
