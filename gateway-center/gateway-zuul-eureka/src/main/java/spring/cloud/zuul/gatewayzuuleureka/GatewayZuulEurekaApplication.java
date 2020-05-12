bc91dd7f9b6a4498f40e639e17923b0d031abcef
Staro <staro.fy.zhao@mail.foxconn.com>
2020-05-12 11:06:52 +0800

    添加了api网关模块

---
+++ gateway-center/gateway-zuul-eureka/src/main/java/spring/cloud/zuul/gatewayzuuleureka/GatewayZuulEurekaApplication.java
@@ -0,0 +1,15 @@
+package spring.cloud.zuul.gatewayzuuleureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy /**支持网关路由**/
+@SpringBootApplication
+public class GatewayZuulEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayZuulEurekaApplication.class, args);
+    }
+
+}
---
+++ gateway-center/gateway-zuul/src/test/java/spring/cloud/zuul/gatewayzuul/GatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.cloud.zuul.gatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
